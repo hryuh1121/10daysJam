bee6a0bf4ad9c725d4b2e0f74f347cd78bcdf8b5
hryuh1121 <h.ryuh1121@gmail.com>
2021-09-20 14:19:51 +0900

    メインシーンに台風とスコア追加

--- gamejam/Assets/honda/TyphoonScript.cs
+++ gamejam/Assets/honda/TyphoonScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TyphoonScript : MonoBehaviour
 {
@@ -136,6 +137,64 @@ public class TyphoonScript : MonoBehaviour
                     speed = 0;
                 }
             }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //jumpF = false;
+
+        Vector3 hitpos = Vector3.zero;
+
+        //当たった座標の取得
+        foreach (ContactPoint2D point in collision.contacts)
+        {
+            hitpos = point.point;
+        }
+
+        BoundsInt.PositionEnumerator position =
+            collision.gameObject.GetComponent<Tilemap>().cellBounds.allPositionsWithin;
+
+        var allPosition = new List<Vector3>();
+
+        //一番近い場所を保存する変数
+        int minPositionNum = 0;
+
+        foreach (var variable in position)
+        {
+            if (collision.gameObject.GetComponent<Tilemap>().GetTile(variable) != null)
+            {
+                allPosition.Add(variable);
+            }
+        }
+
+        //for文で探査する。でも初期化で0入れてるから1からスタート
+        for (int i = 1; i < allPosition.Count; i++)
+        {
+            //それぞれの当たった場所からの大きさを取得、最小を更新したらminPositionNumを更新
+            if ((hitpos - allPosition[i]).magnitude <
+                (hitpos - allPosition[minPositionNum]).magnitude)
+            {
+                minPositionNum = i;
+            }
+        }
+
+        //最終的な位置を一旦格納した。RoundToIntは四捨五入
+        Vector3Int finalPosition = Vector3Int.RoundToInt(allPosition[minPositionNum]);
+
+        TileBase tiletmp = collision.gameObject.GetComponent<Tilemap>().GetTile(finalPosition);
+
+        if (tiletmp != null)
+        {
+            Tilemap map = collision.gameObject.GetComponent<Tilemap>();
+            TilemapCollider2D tileCol = collision.gameObject.GetComponent<TilemapCollider2D>();
+
+            map.SetTile(finalPosition, null);
+            tileCol.enabled = false;
+            tileCol.enabled = true;
+
+
+            //Destroy(Instantiate(particle, finalPosition + Vector3.one * 0.5f, Quaternion.identity), 0.85f);
         }
     }
 }
